ebdae264c9617acb348706ad2918db172414165e
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2017-12-07 18:56:42 +0530

    Finished Ex 2 :)

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -103,11 +103,16 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
-% not need to be normalized.
-price = 0; % You should change this
+% not need to be normalized. 
+test = [1,1650,3];
+for i = 2:size(theta,1)
+  test(i) = (test(i)-mu(i-1))/sigma(i-1);
+endfor
+
+price = test*theta; % You should change this
 
 
-% ============================================================
+% ==================/==========================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
@@ -149,7 +154,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+test = [1,1650,3];
+price = test*theta; % You should change this
 
 
 % ============================================================
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,19 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    for i = 1:size(X,2)
+      temp(i) = theta(i) - (alpha/m)*sum(X(:,i)'*(X*theta-y)); 
+    endfor
     % ============================================================
-
+    
+    theta = temp';
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
 
